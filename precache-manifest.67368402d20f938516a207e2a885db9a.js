62f6951e91938cfe1719c5fe1513adeae2504051
Arpit Jindal <61323807+arpitdhn1998@users.noreply.github.com>
2020-09-05 19:13:33 +0530

    Updates

---
+++ precache-manifest.67368402d20f938516a207e2a885db9a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db1c49fdf3be1d7d09a1379b7e0a3030",
+    "url": "/NetFlix/index.html"
+  },
+  {
+    "revision": "442b2e3ef9b684822f75",
+    "url": "/NetFlix/static/css/main.e276af56.chunk.css"
+  },
+  {
+    "revision": "4cf62c0b347721f1b4c9",
+    "url": "/NetFlix/static/js/2.1812b82c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NetFlix/static/js/2.1812b82c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "442b2e3ef9b684822f75",
+    "url": "/NetFlix/static/js/main.bf15a261.chunk.js"
+  },
+  {
+    "revision": "fa6ff06402c00caff214",
+    "url": "/NetFlix/static/js/runtime-main.1e131b84.js"
+  }
+]);
